e434c4113855723a3fb8cb15cb54cc507b8206ac
Naveen Kumar M N <NaveenKumarMN20@users.noreply.github.com>
2020-09-05 23:40:11 +0530

    Modified FilesController and FilesToDataBaseService to handle multipart files

--- src/main/java/io/tofts/imagefilter/controller/FilesController.java
+++ src/main/java/io/tofts/imagefilter/controller/FilesController.java
@@ -1,9 +1,16 @@
 package io.tofts.imagefilter.controller;
 
+import com.drew.imaging.ImageProcessingException;
 import io.tofts.imagefilter.services.FilesToDataBaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 
 @RestController
@@ -14,11 +21,18 @@ public class FilesController {
     FilesToDataBaseService filesToDataBaseService;
 
     @PostMapping(value = "/addfiles")
-    public ResponseEntity FileProcess(@RequestParam String folderPath) throws Exception {
+    public ResponseEntity FileProcess(@RequestParam MultipartFile[] files) throws IOException, ImageProcessingException {
 
-        if (filesToDataBaseService.getFolderContents(folderPath))
+        int count = 0;
+        for (MultipartFile file : files) {
+            boolean result = filesToDataBaseService.convertFile(file);
+            if (result)
+                count++;
+
+        }
+
+        if (count == files.length)
             return ResponseEntity.ok().build();
-
         return ResponseEntity.notFound().build();
     }
 
--- src/main/java/io/tofts/imagefilter/services/FilesToDataBaseService.java
+++ src/main/java/io/tofts/imagefilter/services/FilesToDataBaseService.java
@@ -1,27 +1,34 @@
 package io.tofts.imagefilter.services;
 
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import io.tofts.imagefilter.mappers.TagToDTO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 @Slf4j
 @Service
 public class FilesToDataBaseService {
 
-    public boolean getFolderContents(String folderPath) {
+    @Autowired
+    TagToDTO tagToDTO;
 
-        File directoryPath = new File(folderPath);
-        log.info("Folder Path" + folderPath);
-        String[] contents = directoryPath.list();
-        if (contents != null) {
-            for (String content : contents) {
-                if (content.contains(".jpg"))
-                    log.info("Files with extension jpg" + content);
-            }
-            return true;
-        }
-        return false;
+    public boolean convertFile(MultipartFile file) throws IOException, ImageProcessingException {
+
+        File convFile = new File(file.getOriginalFilename());
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(file.getBytes());
+        fos.close();
+        tagToDTO.convertJpgTag(ImageMetadataReader.readMetadata(convFile));
+        convFile.delete();
+        return true;
     }
 
 }
+
